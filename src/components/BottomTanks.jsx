a1b3a339cdd0b915c7a5c78a6814737f2f8f8ce4
aleksei <aleksei@example.com>
2025-02-17 20:20:45 +0300

    update17_02

--- src/components/BottomTanks.jsx
+++ src/components/BottomTanks.jsx
@@ -1,13 +1,12 @@
 import Tank from "./Tank";
 
-
 const BottomTanks = () => {
   return (
     <>
       <Tank title="E-322" top={450} left={150} />
       <Tank title="E-326" top={450} left={190 * 5} />
-      <Tank title="E-328" top={450} left={190 * 8} />
-      <Tank title="E-330" top={450} left={190 * 10} />
+      <Tank title="E-328" productType="ЗГПН" top={450} left={190 * 8} />
+      <Tank title="E-330" productType="ЗГПН" top={450} left={190 * 10} />
       <Tank title="E-332" top={450} left={190 * 13} />
       <Tank title="E-334" top={450} left={190 * 15} />
     </>
--- src/components/Tank.jsx
+++ src/components/Tank.jsx
@@ -1,6 +1,6 @@
 import useStore from "../store/store.js";
 
-const Tank = ({ title, top = 0, left = 0 }) => {
+const Tank = ({ title, productType = "ПСТ", top = 0, left = 0 }) => {
   const { selectedTank, setSelectedTank } = useStore();
 
   const handleClick = () => {
@@ -11,11 +11,14 @@ const Tank = ({ title, top = 0, left = 0 }) => {
     <div
       onClick={handleClick}
       className={`w-[150px] h-[150px] rounded-full border-4 flex items-center justify-center text-4xl absolute ${
-        selectedTank === title ? "bg-blue-400" : "bg-white"
+        selectedTank === title ? "bg-green-500" : "bg-white"
       }`}
       style={{ top: `${top}px`, left: `${left}px` }}
     >
-      {title}
+      <div className="flex flex-col justify-center items-center gap-y-1.5">
+        <p>{title}</p>
+        <p className="text-3xl">{productType}</p>
+      </div>
     </div>
   );
 };
