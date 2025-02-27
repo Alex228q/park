39ee875e0aa4bba645ed524044ae7cd9abb71ab1
aleksei <aleksei@example.com>
2025-02-27 20:21:45 +0300

    update27_02

--- src/components/FromPstToTank.jsx
+++ src/components/FromPstToTank.jsx
@@ -8,52 +8,267 @@ import SmallValve from "./sigments/SmallValve";
 const FromPstToTank = () => {
   return (
     <div style={{ position: "absolute" }}>
-      <Line left={2560} length={55} color="#6b79e7" />
-      <Line top={53} isHorizontal left={2537} length={30} color="#6b79e7" />
-      <Valve toUp top={36} left={2507} title={"17"} valveColor={"#6b79e7"} />
-      <Line top={53} isHorizontal left={2497} length={10} color="#6b79e7" />
-      <Line length={49} top={53} left={2497} color="#6b79e7" />
-      <Line isHorizontal length={140} top={95} left={2497} color="#6b79e7" />
-      <Valve toUp top={78} left={2637} title={"100"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={1007} top={95} left={1497} color="#6b79e7" />
-      <Valve toUp top={78} left={1467} title={"51A"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={37} top={95} left={1430} color="#6b79e7" />
-      <Line length={37} top={95} left={1430} color="#6b79e7" />
-      <Line isHorizontal length={37} top={125} left={1400} color="#6b79e7" />
-      <Valve toUp top={108} left={1371} title={"51"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={461} top={125} left={910} color="#6b79e7" />
-      <Line length={25} top={125} left={903} color="#6b79e7" />
-      <Valve top={150} left={897} title={"80"} valveColor={"#6b79e7"} />
-      <Line length={160} top={180} left={903} color="#6b79e7" />
-      <Line isHorizontal length={85} top={333} left={903} color="#6b79e7" />
-      <Line length={9} top={331} left={984} color="#6b79e7" />
-      <Valve top={301} left={978} title={"A"} valveColor={"#6b79e7"} />
-      <Line length={16} top={286} left={984} color="#6b79e7" />
-      <Line length={28} top={333} left={935} color="#6b79e7" />
-      <Arc top={355} left={949} color="#6b79e7" />
-      <Line length={300} top={379} left={935} color="#6b79e7" />
-      <Line isHorizontal length={60} top={679} left={935} color="#6b79e7" />
-      <Valve top={619} left={982} title={"A"} valveColor={"#6b79e7"} />
-      <Line length={37} top={649} left={988} color="#6b79e7" />
-      <Line length={30} top={589} left={988} color="#6b79e7" />
-      <Line isHorizontal length={40} top={95} left={2667} color="#6b79e7" />
-      <Line length={564} top={95} left={2707} color="#6b79e7" />
-      <Arc top={653} left={2721} color="#6b79e7" />
-      <Line isHorizontal length={195} top={355} left={2707} color="#6b79e7" />
-      <Line length={11} top={351} left={2895} color="#6b79e7" />
-      <Valve top={321} left={2889} title={"A"} valveColor={"#6b79e7"} />
-      <Line length={28} top={293} left={2895} color="#6b79e7" />
-      <Line isHorizontal length={39} top={600} left={2707} color="#6b79e7" />
-      <ArcHorizontal top={586} left={2739} color="#6b79e7" />
-      <Line length={53} top={604} left={2763} color="#6b79e7" />
-      <Line isHorizontal length={140} top={653} left={2763} color="#6b79e7" />
-      <Line length={18} top={637} left={2896} color="#6b79e7" />
-      <Valve top={607} left={2890} title={"A"} valveColor={"#6b79e7"} />
-      <Line length={18} top={589} left={2896} color="#6b79e7" />
-      <Line isHorizontal length={201} top={676} left={2523} color="#6b79e7" />
-      <Line length={31} top={652} left={2523} color="#6b79e7" />
-      <Valve top={622} left={2517} title={"A"} valveColor={"#6b79e7"} />
-      <Line length={27} top={595} left={2523} color="#6b79e7" />
+      <Line left={2560} length={30} color="#6b79e7" thickness={4} />
+      <Line
+        top={29}
+        isHorizontal
+        left={2541}
+        length={23}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <SmallValve
+        toUp
+        top={20}
+        left={2526}
+        title={"17"}
+        valveColor={"#6b79e7"}
+      />
+      <Line
+        top={29}
+        isHorizontal
+        left={2516}
+        length={10}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={30} top={29} left={2516} color="#6b79e7" thickness={4} />
+
+      <Line
+        isHorizontal
+        length={982}
+        top={58}
+        left={1650}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <SmallValve
+        toUp
+        top={49}
+        left={2632}
+        title={"100"}
+        valveColor={"#6b79e7"}
+      />
+      <SmallValve
+        toUp
+        top={50}
+        left={1635}
+        title={"51A"}
+        valveColor={"#6b79e7"}
+      />
+      <Line
+        isHorizontal
+        length={20}
+        top={58}
+        left={1615}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={29} top={58} left={1615} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={329}
+        top={83}
+        left={1288}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <SmallValve
+        toUp
+        top={74}
+        left={1273}
+        title={"51"}
+        valveColor={"#6b79e7"}
+      />
+
+      <Line
+        isHorizontal
+        length={342}
+        top={83}
+        left={931}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={50} top={83} left={931} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={40}
+        top={129}
+        left={895}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={20} top={129} left={895} color="#6b79e7" thickness={4} />
+
+      <SmallValve top={149} left={893} title={"80"} valveColor={"#6b79e7"} />
+
+      <Line length={515} top={164} left={895} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={109}
+        top={678}
+        left={895}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <SmallValve top={628} left={1000} title={"A"} valveColor={"#6b79e7"} />
+      <Line length={39} top={643} left={1003} color="#6b79e7" thickness={4} />
+      <Line length={32} top={596} left={1003} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={110}
+        top={358}
+        left={895}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <SmallValve top={335} left={1000} title={"A"} valveColor={"#6b79e7"} />
+      <Line length={12} top={350} left={1003} color="#6b79e7" thickness={4} />
+      <Line length={40} top={295} left={1003} color="#6b79e7" thickness={4} />
+
+      <Line
+        isHorizontal
+        length={70}
+        top={58}
+        left={2647}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={625} top={60} left={2713} color="#6b79e7" thickness={4} />
+
+      <Line
+        isHorizontal
+        length={186}
+        top={355}
+        left={2714}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={14} top={343} left={2896} color="#6b79e7" thickness={4} />
+      <SmallValve top={328} left={2893} title={"A"} valveColor={"#6b79e7"} />
+      <Line length={35} top={293} left={2895} color="#6b79e7" thickness={4} />
+
+      <Line
+        isHorizontal
+        length={188}
+        top={656}
+        left={2713}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <Line length={29} top={631} left={2898} color="#6b79e7" thickness={4} />
+      <SmallValve top={616} left={2895} title={"A"} valveColor={"#6b79e7"} />
+      <Line length={22} top={594} left={2898} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={192}
+        top={681}
+        left={2525}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={42} top={642} left={2525} color="#6b79e7" thickness={4} />
+      <SmallValve top={627} left={2523} title={"A"} valveColor={"#6b79e7"} />
+      <Line length={32} top={595} left={2525} color="#6b79e7" thickness={4} />
+
+      <Line
+        isHorizontal
+        length={426}
+        top={414}
+        left={470}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={20} top={414} left={470} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={31}
+        top={433}
+        left={443}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <SmallValve
+        toUp
+        top={424}
+        left={428}
+        title={"ЗМ-2"}
+        valveColor={"#6b79e7"}
+      />
+
+      <Line
+        isHorizontal
+        length={60}
+        top={433}
+        left={368}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <Line length={187} top={250} left={368} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={20}
+        top={250}
+        left={351}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <SmallValve
+        toUp
+        top={241}
+        left={336}
+        title={"A"}
+        valveColor={"#6b79e7"}
+      />
+
+      <Line
+        isHorizontal
+        length={42}
+        top={250}
+        left={294}
+        color="#6b79e7"
+        thickness={4}
+      />
+
+      <Line length={117} top={298} left={707} color="#6b79e7" thickness={4} />
+      <SmallValve
+        top={283}
+        left={704}
+        title={"ЗМ-7"}
+        valveColor={"#6b79e7"}
+        gapValve="8px"
+      />
+      <Line length={36} top={247} left={707} color="#6b79e7" thickness={4} />
+      <Line
+        isHorizontal
+        length={14}
+        top={247}
+        left={697}
+        color="#6b79e7"
+        thickness={4}
+      />
+      <SmallValve
+        toUp
+        top={238}
+        left={682}
+        title={"A"}
+        valveColor={"#6b79e7"}
+      />
+      <Line
+        isHorizontal
+        length={28}
+        top={247}
+        left={654}
+        color="#6b79e7"
+        thickness={4}
+      />
+      {/* ******************************************************** */}
 
       <Line
         isHorizontal
@@ -148,53 +363,6 @@ const FromPstToTank = () => {
         title={"A"}
         valveColor={"#5fa6e3"}
       />
-
-      <Line isHorizontal length={13} top={414} left={928} color="#6b79e7" />
-      <ArcHorizontal top={401} left={898} color="#6b79e7" />
-      <Line isHorizontal length={13} top={414} left={890} color="#6b79e7" />
-
-      <ArcHorizontal top={401} left={864} color="#6b79e7" />
-      <Line isHorizontal length={102} top={414} left={768} color="#6b79e7" />
-
-      <ArcHorizontal top={401} left={744} color="#6b79e7" />
-      <Line isHorizontal length={60} top={414} left={690} color="#6b79e7" />
-      <Line length={19} top={401} left={690} color="#6b79e7" />
-      <Arc top={377} left={704} color="#6b79e7" />
-
-      <Line length={78} top={305} left={684} color="#6b79e7" />
-      <Valve
-        top={275}
-        left={677}
-        title={"ЗМ-7"}
-        valveColor={"#6b79e7"}
-        gapValve="8px"
-      />
-      <Line length={10} top={265} left={683} color="#6b79e7" />
-      <Valve toUp top={232} left={660} title={"A"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={12} top={250} left={648} color="#6b79e7" />
-      <Line isHorizontal length={23} top={264} left={683} color="#6b79e7" />
-      <Line length={13} top={254} left={699} color="#6b79e7" />
-      <Line isHorizontal length={16} top={249} left={690} color="#6b79e7" />
-      <Line isHorizontal length={81} top={442} left={616} color="#6b79e7" />
-      <Line length={29} top={416} left={690} color="#6b79e7" />
-      <ArcHorizontal top={425} left={592} color="#6b79e7" />
-      <Line isHorizontal length={149} top={442} left={450} color="#6b79e7" />
-      <Valve toUp top={424} left={420} title={"ЗМ-2"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={54} top={442} left={367} color="#6b79e7" />
-      <Line length={192} top={257} left={367} color="#6b79e7" />
-      <Line isHorizontal length={11} top={257} left={358} color="#6b79e7" />
-      <Valve toUp top={239} left={329} title={"A"} valveColor={"#6b79e7"} />
-      <Line isHorizontal length={40} top={257} left={289} color="#6b79e7" />
-
-      <Line left={2188} top={95} length={130} color="#6b79e7" />
-      <Line isHorizontal left={2188} top={219} length={30} color="#6b79e7" />
-      <Valve toUp top={202} left={2217} title={"59"} valveColor={"#6b79e7"} />
-      <Line isHorizontal left={2247} top={219} length={30} color="#6b79e7" />
-      <Line left={2270} top={219} length={148} color="#6b79e7" />
-      <Line isHorizontal left={2270} top={360} length={230} color="#6b79e7" />
-      <Line left={2493} top={351} length={13} color="#6b79e7" />
-      <Valve top={321} left={2488} title={"A"} valveColor={"#6b79e7"} />
-      <Line left={2493} top={279} length={42} color="#6b79e7" />
     </div>
   );
 };
--- src/components/FromZgpnToTank.jsx
+++ src/components/FromZgpnToTank.jsx
@@ -1,81 +1,161 @@
-import Arc from "./Arc";
-import ArcHorizontal from "./ArcHorizontal";
 import Line from "./sigments/Line";
-import Valve from "./sigments/Valve";
+import SmallValve from "./sigments/SmallValve";
 
 const FromZgpnToTank = () => {
   return (
     <div style={{ position: "absolute" }}>
-      <Line left={1841} length={41} color="#bb34e0" />
-      <Arc left={1855} top={35} color="#bb34e0" />
-      <Line left={1841} length={31} top={59} color="#bb34e0" />
-      <Arc left={1855} top={84} color="#bb34e0" />
-      <Line isHorizontal left={1815} length={26} top={144} />
-      <Valve toUp top={127} left={1785} title="134" />
-      <Line isHorizontal left={1743} length={42} top={144} />
+      <Line left={1848} length={93} color="#bb34e0" thickness={4} />
+      <Line isHorizontal left={1834} top={83} length={14} thickness={4} />
+      <SmallValve toUp left={1819} top={75} title={"134"} />
+      <Line isHorizontal left={1786} top={83} length={33} thickness={4} />
 
-      <Line left={1841} length={62} top={108} color="#bb34e0" />
+      <SmallValve
+        left={1845}
+        top={93}
+        title={"135"}
+        valveColor="#bb34e0"
+        gapValve="1px"
+      />
 
-      <Valve top={170} left={1835} valveColor="#bb34e0" title="135" />
-      <Line left={1841} length={42} top={200} color="#bb34e0" />
-      <Line isHorizontal left={1815} length={33} top={238} color="#bb34e0" />
-      <Valve toUp top={220} left={1785} title="C-1" valveColor="#bb34e0" />
-      <Line isHorizontal left={1740} length={45} top={238} color="#bb34e0" />
-      <Line left={1736} length={46} top={199} color="#bb34e0" />
-      <Valve top={170} left={1730} valveColor="#bb34e0" title="136" />
-      <Line left={1736} length={57} top={113} color="#bb34e0" />
-      <Line isHorizontal left={1688} length={55} top={113} color="#bb34e0" />
-      <Line left={1684} length={12} top={113} color="#bb34e0" />
-      <Valve top={125} left={1678} valveColor="#bb34e0" title="137" />
-      <Line isHorizontal left={1656} length={35} top={160} color="#bb34e0" />
-      <Line left={1656} length={28} top={139} color="#bb34e0" />
-      <Line isHorizontal left={1548} length={115} top={139} color="#bb34e0" />
-      <Valve toUp top={121} left={1518} valveColor="#bb34e0" title="53" />
-      <Line isHorizontal left={1488} length={30} top={139} color="#bb34e0" />
-      <Line left={1488} length={70} top={139} color="#bb34e0" />
-      <Line isHorizontal left={1415} length={80} top={203} color="#bb34e0" />
-      <Valve toUp top={186} left={1386} valveColor="#bb34e0" title="55" />
-      <Line isHorizontal left={1366} length={20} top={203} color="#bb34e0" />
-      <Line left={1366} length={463} top={203} color="#bb34e0" />
-      <Line isHorizontal left={1366} length={62} top={664} color="#bb34e0" />
-      <ArcHorizontal top={651} left={1421} color="#bb34e0" />
-      <Line isHorizontal left={1445} length={117} top={664} color="#bb34e0" />
-      <Line left={1560} length={29} top={642} color="#bb34e0" />
-      <Valve top={612} left={1554} valveColor="#bb34e0" title="A" />
-      <Line left={1560} length={24} top={588} color="#bb34e0" />
+      <Line left={1848} length={15} top={108} color="#bb34e0" thickness={4} />
+      <Line
+        isHorizontal
+        left={1834}
+        length={15}
+        top={119}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <SmallValve
+        toUp
+        left={1819}
+        top={111}
+        title={"С-1"}
+        valveColor="#bb34e0"
+      />
+      <Line
+        isHorizontal
+        left={1783}
+        length={36}
+        top={119}
+        color="#bb34e0"
+        thickness={4}
+      />
 
-      <Line isHorizontal left={1369} length={209} top={350} color="#bb34e0" />
-      <Line left={1573} length={9} top={348} color="#bb34e0" />
-      <Valve top={318} left={1567} valveColor="#bb34e0" title="A" />
-      <Line left={1573} length={22} top={296} color="#bb34e0" />
+      <Line left={1782} length={15} top={108} color="#bb34e0" thickness={4} />
 
-      <Line left={1684} length={194} top={155} color="#bb34e0" />
-      <Line isHorizontal left={1684} length={277} top={348} color="#bb34e0" />
-      <Valve top={318} left={1945} valveColor="#bb34e0" title="A" />
-      <Line left={1951} length={24} top={294} color="#bb34e0" />
-      <Line left={1770} length={300} top={349} color="#bb34e0" />
-      <Line isHorizontal left={1770} length={36} top={645} color="#bb34e0" />
-      <ArcHorizontal left={1799} top={628} color="#bb34e0" />
-      <Line isHorizontal left={1823} length={128} top={645} color="#bb34e0" />
+      <SmallValve
+        left={1779}
+        top={93}
+        title={"136"}
+        valveColor="#bb34e0"
+        gapValve="1px"
+      />
+      <Line left={1782} length={10} top={83} color="#bb34e0" thickness={4} />
+      <Line
+        isHorizontal
+        left={1749}
+        length={35}
+        top={83}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <Line left={1747} length={29} top={83} color="#bb34e0" thickness={4} />
 
-      <Valve top={610} left={1942} valveColor="#bb34e0" title="A" />
-      <Line left={1948} length={12} top={640} color="#bb34e0" />
-      <Line left={1948} length={15} top={595} color="#bb34e0" />
-      <Line isHorizontal left={1684} length={210} top={280} color="#bb34e0" />
-      <Line left={1891} length={147} top={140} color="#bb34e0" />
-      <Line isHorizontal left={1891} length={147} top={140} color="#bb34e0" />
-      <Valve toUp top={123} left={2037} valveColor="#bb34e0" title="57" />
-      <Line isHorizontal left={2067} length={50} top={140} color="#bb34e0" />
-      <Line left={2110} length={35} top={140} color="#bb34e0" />
-      <Line isHorizontal left={2110} length={72} top={168} color="#bb34e0" />
-      <ArcHorizontal top={155} left={2176} color="#bb34e0" />
-      <Line isHorizontal left={2200} length={18} top={168} color="#bb34e0" />
-      <Valve toUp top={151} left={2217} valveColor="#bb34e0" title="58" />
-      <Line isHorizontal left={2247} length={28} top={168} color="#bb34e0" />
-      <Line left={2274} length={195} top={168} color="#bb34e0" />
-      <Line isHorizontal left={2274} length={222} top={356} color="#bb34e0" />
-      <Line left={2490} length={12} top={351} color="#bb34e0" />
-      <Line left={2490} length={47} top={274} color="#bb34e0" />
+      <SmallValve
+        left={1744}
+        top={112}
+        title={"137"}
+        valveColor="#bb34e0"
+        gapValve="1px"
+      />
+      <Line left={1747} length={18} top={127} color="#bb34e0" thickness={4} />
+      <Line
+        isHorizontal
+        left={1730}
+        length={21}
+        top={142}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <Line isHorizontal left={1730} length={17} top={83} thickness={4} />
+
+      <Line isHorizontal left={1694} length={21} top={83} thickness={4} />
+
+      <SmallValve toUp left={1715} top={75} title={"147"} />
+      <Line left={1694} length={31} top={83} thickness={4} />
+      <Line isHorizontal left={1686} length={9} top={83} thickness={4} />
+      <Line isHorizontal left={1686} length={9} top={142} thickness={4} />
+
+      <SmallValve
+        toUp
+        left={1715}
+        top={133}
+        title={"53"}
+        valveColor="#bb34e0"
+      />
+
+      <Line
+        isHorizontal
+        left={1694}
+        length={21}
+        top={142}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <Line left={1694} length={27} top={116} color="#bb34e0" thickness={4} />
+      <Line
+        isHorizontal
+        left={1686}
+        length={12}
+        top={114}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <SmallValve toUp left={1671} top={75} title={"52"} />
+      <Line isHorizontal left={1619} length={52} top={83} thickness={4} />
+
+      <SmallValve
+        toUp
+        left={1671}
+        top={105}
+        title={"55"}
+        valveColor="#bb34e0"
+      />
+      <SmallValve toUp left={1671} top={133} title={"148"} />
+      <Line
+        isHorizontal
+        left={1410}
+        length={261}
+        top={114}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <Line left={1410} length={550} top={114} color="#bb34e0" thickness={4} />
+      <Line
+        isHorizontal
+        left={1410}
+        length={175}
+        top={662}
+        color="#bb34e0"
+        thickness={4}
+      />
+
+      <Line left={1583} length={31} top={635} color="#bb34e0" thickness={4} />
+      <SmallValve left={1580} top={620} title={"A"} valveColor="#bb34e0" />
+      <Line left={1583} length={23} top={597} color="#bb34e0" thickness={4} />
+
+      <Line
+        isHorizontal
+        left={1410}
+        length={175}
+        top={342}
+        color="#bb34e0"
+        thickness={4}
+      />
+      <Line left={1583} length={10} top={336} color="#bb34e0" thickness={4} />
+      <SmallValve left={1580} top={321} title={"A"} valveColor="#bb34e0" />
+      <Line left={1583} length={23} top={298} color="#bb34e0" thickness={4} />
     </div>
   );
 };
